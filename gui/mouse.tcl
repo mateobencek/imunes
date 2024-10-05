dbe1d05f875282d276578a2a7ce819e1c3e05c15
Denis Salopek <denisSal@users.noreply.github.com>
2024-10-05 22:30:37 +0200

    Connect to stolen ifaces only on rj45 nodes

--- gui/mouse.tcl
+++ gui/mouse.tcl
@@ -703,6 +703,10 @@ proc button3node { c x y } {
 
 	set from_iface_id $this_iface_id
 	if { [getIfcType $node_id $this_iface_id] == "stolen" } {
+	    if { [getNodeType $node_id] != "rj45" } {
+		continue
+	    }
+
 	    set from_iface_label "$this_iface_id - \[[getIfcName $node_id $this_iface_id]\]"
 	} else {
 	    set from_iface_label [getIfcName $node_id $this_iface_id]
@@ -743,6 +747,10 @@ proc button3node { c x y } {
 
 		    set to_iface_id $other_iface_id
 		    if { [getIfcType $peer_node_id $other_iface_id] == "stolen" } {
+			if { [getNodeType $peer_node_id] != "rj45" } {
+			    continue
+			}
+
 			set to_iface_label "$other_iface_id - \[[getIfcName $peer_node_id $other_iface_id]\]"
 		    } else {
 			set to_iface_label [getIfcName $peer_node_id $other_iface_id]
